791c8df4e4714c81b398ea565dd4dba22856bd15
Ibrahim Hamwi <ibrahimhamwi_619@hotmail.com>
2022-05-14 19:05:27 +0300

    trying to instatiate prefabs

--- assets/Scripts/Klondike.ts
+++ assets/Scripts/Klondike.ts
@@ -1,19 +1,19 @@
-import { _decorator, Component, Node } from 'cc';
+import { _decorator, Component, Node, SpriteFrame, Prefab, instantiate, Sprite, resources, quat, math } from 'cc';
 const { ccclass, property } = _decorator;
 
 @ccclass('Klondike')
 export class Klondike extends Component {
 
-    // public static string[] suits = ["C", "D", "H", "S"];
+    // sprite array for card faces
+    @property({ type: SpriteFrame }) cardFaces: SpriteFrame[] = [];
+    @property({ type: Prefab }) card: Prefab = null;
     public static suits: string[] = ["C", "D", "H", "S"];
     public static values: string[] = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
 
     public static deck: string[] = [];
 
-
-
     start() {
-        this.PlayCards();
+        this.PlayCards(); // call the function to play cards
     }
 
     update(deltaTime: number) {
@@ -21,12 +21,13 @@ export class Klondike extends Component {
     }
 
     PlayCards(): void {
-        Klondike.deck = Klondike.GenerateDeck();
-        Klondike.deck = Klondike.ShuffleDeck(Klondike.deck);
+        Klondike.deck = Klondike.GenerateDeck(); // generate the deck
+        Klondike.deck = Klondike.ShuffleDeck(Klondike.deck);// shuffle the deck
         Klondike.deck.forEach(card => {
-            console.log(card);
+            // console.log(card);
         }
         );
+        this.KlondikeDeal(); // deal the cards
     }
 
     static ShuffleDeck(deck: string[]): string[] {
@@ -39,8 +40,6 @@ export class Klondike extends Component {
         }
         return deck;
     }
-
-
     static GenerateDeck(): string[] {
         Klondike.deck = [];
         for (let suit of Klondike.suits) {
@@ -49,6 +48,18 @@ export class Klondike extends Component {
             }
         }
         return Klondike.deck;
+    }
+    KlondikeDeal(): void {
+        // deal the cards
+        Klondike.deck.forEach(card => {
+            let newCard = instantiate(this.card);
+            newCard.setRotation(math.quat(0, 0, 0, 1));
+            newCard.setPosition(0, 0);
+            newCard.name = card;
+            console.log(card);
+
+        }
+        );
     }
 }
 
